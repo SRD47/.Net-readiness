f591602f596e98a60bbd4a8bfbee5e20bd07dcdf
LOQ <LOQ@DESKTOP-TSSELJ1>
2025-07-09 16:31:14 +0545

    Added data annotations

--- Practice/Models/Entitites/Product.cs
+++ Practice/Models/Entitites/Product.cs
@@ -1,4 +1,7 @@
-﻿namespace Practice.Models.Entities
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Practice.Models.Entities
 {
     public enum Currency
     {
@@ -8,12 +11,27 @@
 
     public class Product
     {
+        [Key]
         public Guid ID { get; set; } = Guid.NewGuid();
-        public string Name { get; set; } = "";
-        public string Category { get; set; } = "";
-        public string Class { get; set; } = "";
+
+        [Required(ErrorMessage = "Product name is required.")]
+        [StringLength(20, MinimumLength = 6, ErrorMessage = "Name must be between 6 and 20 characters.")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Category is required.")]
+        public string Category { get; set; }
+
+        [Required(ErrorMessage = "Class is required.")]
+        public string Class { get; set; }
+
+        [Required(ErrorMessage = "Quantity is required.")]
+        [Range(1, int.MaxValue, ErrorMessage = "Quantity must be greater than 0.")]
         public int Quantity { get; set; }
-        public string Price { get; set; } = "";
+
+        [Required(ErrorMessage = "Price is required.")]
+        public string Price { get; set; }
+
+        [Required(ErrorMessage = "Currency is required.")]
         public Currency Currency { get; set; }
     }
 }
